bda67bf898835dd4c163ba1325296ed6775b512a
TuyenToMi <tominhtuyen18092k4@gmail.com>
2024-09-03 18:15:28 +0700

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-
+app.use(morgan('combined'))
 
 //route
 app.get('/tin-tuc', (req, res) => {
-    var a = 1;
-    var b = 2;
-
-    var c = a + b;
     res.send('Hello World!')
 }
 )
